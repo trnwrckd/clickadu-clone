3585603c72cc541c0bb4428cb3d3358738ecc173
Ishmamur Rahman <trnwrckd@gmail.com>
2021-08-09 04:17:21 +0600

    homepage except accordion

---
+++ js/index-slick.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function () {
+    $('.partners , .locations').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
